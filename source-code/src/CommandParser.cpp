448386080bd621c47f566c58224e4c3445847f8a
Lucas Hubner <lucasguilhermehub@gmail.com>
2016-07-29 02:43:13 -0300

    Update

--- source-code/src/CommandParser.cpp
+++ source-code/src/CommandParser.cpp
@@ -14,8 +14,17 @@ void CommandParser::parseCommand(char *command){
     if(command[0] == 'f' && command[1] == 'w'){
         this->motor->forward();
     }
+
     else if(command[0]=='b' && command[1] =='w'){
         this->motor->backward();
+    }
+
+    else if(command[0] == 't' && command[1] 'l'){
+        this->motor->rigtWheelForward();
+    }
+
+    else if(command[0] == 't' && command[1] 'r'){
+        this->motor->leftWheelForward();
     }
 
 }
